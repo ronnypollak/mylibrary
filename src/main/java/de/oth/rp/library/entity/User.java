133cbb76386ada5114ae0a36a71e705cbebff842
ronnypollak <ronnymailpollak@gmail.com>
2022-01-19 17:52:18 +0100

    redesign bookdetail page and added main.css

--- src/main/java/de/oth/rp/library/entity/User.java
+++ src/main/java/de/oth/rp/library/entity/User.java
@@ -2,25 +2,74 @@ package de.oth.rp.library.entity;
 
 
 import de.oth.rp.library.entity.util.SingleIdEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class User extends SingleIdEntity<Long> {
+public class User extends SingleIdEntity<String> implements UserDetails {
 
-    @Id @GeneratedValue
-    private long userId;
+//    @Id @GeneratedValue
+//    private long userId;
+    @Id
+    @Column(nullable = false, unique = true)
     private String username;
-    @ElementCollection
-    private List<String> ranks;
+    @Column(nullable = false)
+    private String password;
+//    @ElementCollection
+//    private List<String> ranks;
+    private final String role = "ADMIN";
     private int bookCount;
     private int downloadCount;
     @OneToMany
     private List<Book> ownedBooks;
 
     @Override
-    public Long getID() {
-        return this.userId;
+    public String getID() {
+        return this.username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return User.this.role;
+            }
+        });
+    }
+
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
